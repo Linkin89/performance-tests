e4fc063ccc090f3a7728eb18d5ec4918400a9f6f
Linkin89 <vadimlinkin@gmail.com>
2025-12-20 22:48:33 +0500

    Практика реализации HTTP API клиентов: /api/v1/operations

---
+++ clients/http/gateway/operations/client.py
@@ -0,0 +1,185 @@
+from typing import Literal, TypedDict
+
+from httpx import Response
+from clients.http.client import HTTPClient
+
+
+class GetOperationIdPathDict(TypedDict):
+    """Структура данных для получения информацци об операции."""
+    operation_id: str
+
+
+class GetOperationReceiptPathDict(TypedDict):
+    """Структура данных для получения чека по операции."""
+    operation_id: str
+
+
+class GetOperationsQueryDict(TypedDict):
+    """Структура данных для получения списка операций для определенного счета."""
+    accountId: str
+
+
+class GetOperationsSummaryQueryDict(TypedDict):
+    """Структура данных для получения статистики по операциям для определенного счета."""
+    accountId: str
+
+
+class MakeFeeOperationBodyDict(TypedDict):
+    """Структура данных для создания операции комиссии."""
+    status: Literal['FAILED', 'COMPLETED', 'IN_PROGRESS', 'UNSPECIFIED']
+    amount: int
+    cardId: str
+    accountId: str
+
+
+class MakeTopUpOperationBodyDict(TypedDict):
+    """Структура данных для создания операции пополнения."""
+    status: Literal['FAILED', 'COMPLETED', 'IN_PROGRESS', 'UNSPECIFIED']
+    amount: int
+    cardId: str
+    accountId: str
+
+
+class MakeCashbackOperationBodyDict(TypedDict):
+    """Структура данных для создания операции кэшбэка."""
+    status: Literal['FAILED', 'COMPLETED', 'IN_PROGRESS', 'UNSPECIFIED']
+    amount: int
+    cardId: str
+    accountId: str
+
+
+class MakeTransferOperationBodyDict(TypedDict):
+    """Структура данных для создания операции перевода."""
+    status: Literal['FAILED', 'COMPLETED', 'IN_PROGRESS', 'UNSPECIFIED']
+    amount: int
+    cardId: str
+    accountId: str
+
+
+class MakePurchaseOperationBodyDict(TypedDict):
+    """Структура данных для создания операции покупки."""
+    status: Literal['FAILED', 'COMPLETED', 'IN_PROGRESS', 'UNSPECIFIED']
+    amount: int
+    cardId: str
+    accountId: str
+    category: str
+
+
+class MakeBillPaymentOperationBodyDict(TypedDict):
+    """Структура данных для создания операции оплаты по счету."""
+    status: Literal['FAILED', 'COMPLETED', 'IN_PROGRESS', 'UNSPECIFIED']
+    amount: int
+    cardId: str
+    accountId: str
+
+
+class MakeCashWithdrawalOpertionBodyDict(TypedDict):
+    """Структура данных для создания операции снятия наличных денег."""
+    status: Literal['FAILED', 'COMPLETED', 'IN_PROGRESS', 'UNSPECIFIED']
+    amount: int
+    cardId: str
+    accountId: str
+
+
+class OperationsGatewayHTTPClient(HTTPClient):
+    """
+    Клиент для взаимодействия c /api/v1/operations сервиса http-gateway.
+    """
+
+    def get_operation_api(self, operation_id: GetOperationIdPathDict) -> Response:
+        """
+        Получение информации об операции по `operation_id`.
+
+        :param operation_id: id операции
+        :returns Response: объект httpx.Response
+        """
+        return self.client.get(url=f'/api/v1/operations/{operation_id}')
+
+    def get_operation_receipt_api(self, operation_id: GetOperationReceiptPathDict) -> Response:
+        """
+        Получение чека по операции по `operation_id`.
+
+        :param operation_id: id операции
+        :returns Response: объект httpx.Response
+        """
+        return self.client.get(url=f'/api/v1/operations/operation-receipt/{operation_id}')
+
+    def get_operations_api(self, query: GetOperationsQueryDict) -> Response:
+        """
+        Получение списка операций для определенного счета.
+
+        :param accountId: id аккаунта
+        :returns Response: объект httpx.Response
+        """
+        return self.client.get(url='/api/v1/operations/operations/', query=query)
+
+    def get_opertions_summary_api(self, query: GetOperationsSummaryQueryDict) -> Response:
+        """
+        Получение статистики по операциям для определенного счета.
+
+        :param accountId: id аккаунта
+        :returns Response: объект httpx.Response
+        """
+        return self.client.get(url='/api/v1/operations/operations-summary', query=query)
+
+    def make_fee_operation_api(self, body: MakeFeeOperationBodyDict) -> Response:
+        """
+        Создание операции комиссии.
+
+        :param json: данные для создания операции комиссии
+        :returns Request: объект https.Response
+        """
+        return self.client.post(url='/api/v1/operations/make-fee-operation', json=body)
+
+    def make_top_up_operation_api(self, body: MakeTopUpOperationBodyDict) -> Response:
+        """
+        Создание операции пополнения.
+        :param json: данные для создания операции пополнения
+        :returns Request: объект https.Response
+        """
+        return self.client.post(url='/api/v1/operations/make-top-up-operation', json=body)
+
+    def make_cashback_operation_api(self, body: MakeCashbackOperationBodyDict) -> Response:
+        """
+        Создание операции кэшбэка.
+
+        :param json: данные для создания операции кэшбэка
+        :returns Request: объект https.Response
+        """
+        return self.client.post(url='/api/v1/operations/make-cashback-operation', json=body)
+
+    def make_transfer_operation_api(self, body: MakeTransferOperationBodyDict) -> Response:
+        """
+        Создание операции перевода.
+
+        :param json: данные для создания операции перевода
+        :returns Request: объект https.Response
+        """
+        return self.client.post(url='/api/v1/operations/make-transfer-operation', json=body)
+
+    def make_purchase_operation_api(self, body: MakePurchaseOperationBodyDict) -> Response:
+        """
+        Создание операции покупки.
+
+        :param json: данные для создания операции покупки
+        :returns Request: объект https.Response
+        """
+        return self.client.post(url='/api/v1/operations/make-purchase-operation', json=body)
+
+    def make_bill_payment_operation_api(self, body: MakeBillPaymentOperationBodyDict) -> Response:
+        """
+        Создание операции оплаты по счету.
+
+        :param json: данные для создания операции оплаты по счёту
+        :returns Request: объект https.Response
+        """
+        return self.client.post(url='/api/v1/operations/make-bill-payment-operation', json=body)
+
+    def make_cash_withdrawal_operation_api(self, body: MakeCashWithdrawalOpertionBodyDict) -> Response:
+        """
+        Создание операции снятия наличных денег.
+
+        :param json: данные для создания операции оплаты по счёту
+        :returns Request: объект https.Response
+        """
+        return self.client.post(url='/api/v1/operations/make-cash-withdrawal-operation', json=body)
