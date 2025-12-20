bb02d0864d4a84635407aecb46067fda5830d565
Linkin89 <vadimlinkin@gmail.com>
2025-12-20 20:50:48 +0500

    Практика: Написание HTTP API клиента

---
+++ clients/http/gateway/cards/client.py
@@ -0,0 +1,36 @@
+from typing import TypedDict
+from httpx import Response
+from clients.http.client import HTTPClient
+
+
+class IssueVirtualCardDict(TypedDict):
+    userId: str
+    accountId: str
+
+
+class IssuePhysicalCardDict(TypedDict):
+    userId: str
+    accountId: str
+
+
+class CardsGatewayHTTPClient(HTTPClient):
+    """
+    Клиент для взаимодействия с /api/v1/cards сервиса http-gateway.
+    """
+    def issue_virtual_card_api(self, json: IssueVirtualCardDict) -> Response:
+        """
+        Создать виртуальную карту для пользователя по его userId и accountId.
+
+        :param json: json содержащий userId и accountId
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.client.post(url='/api/v1/cards/issue-virtual-card', json=json)
+
+    def issue_physical_card_api(self, json: IssuePhysicalCardDict) -> Response:
+        """
+        Создать физическую карту для пользователя по его userId и accountId.
+
+        :param json: json содержащий userId и accountId
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.client.post(url='/api/v1/cards/issue-physical-card', json=json)
