85e80229a99cd0a59eef48cca9ae2f29dec2f18b
Linkin89 <vadimlinkin@gmail.com>
2026-01-11 18:33:21 +0500

    Практикуемся в написании Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -17,10 +17,10 @@ class CreateUserSchema(BaseModel):
     model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
 
     email: EmailStr
-    lastName: str
-    firstName: str
-    middleName: str
-    phoneNumber: str
+    last_name: str
+    first_name: str
+    middle_name: str
+    phone_number: str
 
 
 class CreateUserResponseSchema(BaseModel):
