2193e34f9c64cf6a0dd7606089bb68ffd649fc6e
Linkin89 <vadimlinkin@gmail.com>
2026-01-25 21:53:38 +0500

    Практика использования API-клиентов: получение документов по счету

---
+++ grpc_api_client_get_documents.py
@@ -0,0 +1,23 @@
+from clients.grpc.gateway.accounts.client import build_accounts_gateway_grpc_client
+from clients.grpc.gateway.documents.client import build_documents_gateway_grpc_client
+from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
+
+
+users_gateway_client = build_users_gateway_grpc_client()
+account_gateway_client = build_accounts_gateway_grpc_client()
+documents_gateway_client = build_documents_gateway_grpc_client()
+
+
+response_create_user = users_gateway_client.create_user()
+print('Create user response: ', response_create_user)
+
+user_id = response_create_user.user.id
+response_open_credit_card_account = account_gateway_client.open_credit_card_account(user_id)
+print('Open credit card account response: ', response_open_credit_card_account)
+
+account_id = response_open_credit_card_account.account.id
+response_get_tariff_document = documents_gateway_client.get_tariff_document(account_id)
+print('Get tariff document response: ', response_get_tariff_document)
+
+response_get_contract_document = documents_gateway_client.get_contract_document(account_id)
+print('Get contract document response: ', response_get_contract_document)
---
+++ grpc_api_client_get_user.py
@@ -0,0 +1,10 @@
+from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
+
+
+users_gateway_client = build_users_gateway_grpc_client()
+
+create_user_response = users_gateway_client.create_user()
+
+user_id = create_user_response.user.id
+response_get_user = users_gateway_client.get_user(user_id)
+print(response_get_user)
