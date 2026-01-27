be693485def93c1383a8b4189b904fb49995d15e
Linkin89 <vadimlinkin@gmail.com>
2026-01-27 17:37:10 +0500

    Практика: Создание билдеров для HTTP API клиентов Locust

--- clients/http/gateway/operations/client.py
+++ clients/http/gateway/operations/client.py
@@ -1,7 +1,9 @@
 from httpx import QueryParams, Response
+from locust.env import Environment
 from pydantic import UUID4
 from clients.http.client import HTTPClient, HTTPClientExtensions
-from clients.http.gateway.client import build_gateway_http_client
+from clients.http.gateway.client import (build_gateway_http_client,
+                                         build_gateway_locust_http_client)
 from clients.http.gateway.operations.schema import (
     GetOperationReceiptResponseSchema,
     GetOperationResponseSchema,
@@ -196,3 +198,16 @@ class OperationsGatewayHTTPClient(HTTPClient):
 
 def build_operations_gateway_http_client() -> OperationsGatewayHTTPClient:
     return OperationsGatewayHTTPClient(build_gateway_http_client())
+
+
+def build_operations_gateway_locust_http_client(environment: Environment) -> OperationsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр OperationsGatewayHTTPClient адаптированного под Locust.
+
+    Клиент автоматически собирает метрики и передаёт их в Locust через хуки.
+    Используется исключительно в нагрузочных тестах.
+
+    :param environment: объект окружения Locust.
+    :return: экземпляр OperationsGatewayHTTPClient с хуками сбора метрик.
+    """
+    return OperationsGatewayHTTPClient(client=build_gateway_locust_http_client(environment))
